a04a763d50192d956ca31edb17b462d8a7653303
repoleved08 <geekbii08@gmail.com>
2024-04-16 14:46:28 +0300

    vue-level3-saas

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import Navbar from './components/Navbar.vue';
+import Hero from './components/Hero.vue';
+</script>
+
+<template>
+  <header>
+    <Navbar />
+  </header>
+  <main>
+    <Hero />
+  </main>
+
+  <RouterView />
+</template>
+
